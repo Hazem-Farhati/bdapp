3e2b43ac3998e706719e68166743451399ea4959
Hazem-Farhati <79176361+Hazem-Farhati@users.noreply.github.com>
2021-06-20 10:40:38 +0100

    mh

--- plate-ang/src/app/cours/courdetails/courdetails.component.ts
+++ plate-ang/src/app/cours/courdetails/courdetails.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-courdetails',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courdetails.component.css']
 })
 export class CourdetailsComponent implements OnInit {
+  urlSafe: SafeResourceUrl;
+  url: string = "../../../assets/images/blog/r.mp4"
 
-  constructor() { }
+  constructor(public sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
+    this.urlSafe= this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
   }
 
 }
